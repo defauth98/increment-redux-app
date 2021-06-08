f43249ab90d69ecd98a8138e9cbaca4aa66831fb
defauth98 <neto.daniribeiro@gmail.com>
2021-06-08 11:11:21 -0300

    configurando o redux com react-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </Provider>
   );
 }
 
